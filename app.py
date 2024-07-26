dbf5018948b6d230fb46163a6cc5e557c1e4e0b7
gitpds <pds@pds.dev>
2024-07-25 21:41:42 -0500

    Test

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
+# from twilio.twiml.messaging_response import MessagingResponse
 import os
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['GET', 'POST'])
+# @app.route("/sms", methods=['GET', 'POST'])
+# def sms_reply():
+#     """Respond to incoming messages with a friendly SMS."""
+#     # Start our TwiML response
+#     response = MessagingResponse()
+
+#     # Add a message
+#     response.message("The Robots are coming! Head for the hills!")
+
+#     return str(response)
+
+
+
+@app.route("/", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming messages with a friendly SMS."""
-    # Start our TwiML response
-    response = MessagingResponse()
+    test = 'test'
 
-    # Add a message
-    response.message("The Robots are coming! Head for the hills!")
+    return test
 
-    return str(response)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
